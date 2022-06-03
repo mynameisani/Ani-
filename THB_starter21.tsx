4719f48fe0981d90cde5e5c4d46cba60ab0f7580
mynameisani <nazanjanani@gmail.com>
2022-06-03 09:55:00 +0200

    Moin

---
+++ THB_starter21.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="THB_starter21" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="src/assets/THB_starter21.png" width="1024" height="1024"/>
+</tileset>
